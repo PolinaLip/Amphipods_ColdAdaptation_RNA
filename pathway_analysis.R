ad250a13f31f428970ec87c1c3d408f41a455f82
PolinaLip <polina.lipaeva@gmail.com>
2020-07-30 17:13:37 +0200

    Cleaning up

--- pathway_analysis.R
+++ pathway_analysis.R
@@ -1,23 +1,22 @@
 #BiocManager::install("pathview")
 library(pathview)
 library(dplyr)
-data(gse16873.d)
+data(gse16873.d) # test data
 
-# example from pathview
+### Example from pathview package
 pv.out <- pathview(gene.data = gse16873.d[, 1], pathway.id = "04110",
                    species = "hsa", out.suffix = "gse16873")
 
-# analysis
+### Prepare data for analysis 
 pathway_dir <- 'labeglo2/Transcriptomics/annotations/pathway_analisys/Gla/Gla_KEGG_annotation/kegg_annot_assembly/'
-#pathways_to_analyse <- read.table(file.path(pathway_dir, 'pathway_to_analyse2.txt'), 
-#                                sep = '\t', header = F, colClasses = "character")
-#pathways_to_analyse <- subset(pathways_to_analyse, V1 != '04723') # for Gla
+pathways_to_analyse <- read.table(file.path(pathway_dir, 'pathway_to_analyse2.txt'), # I manually chose the pathways I want to check
+                                sep = '\t', header = F, colClasses = "character")
+pathways_to_analyse <- subset(pathways_to_analyse, V1 != '04723') # for Gla
+
 de_results <- res # res from DE_analysis.R
 lfc_all <- ifelse(de_results$padj > 0.05 | is.na(de_results$padj), 
                   0, de_results$log2FoldChange)
 names(lfc_all) <- row.names(res)
-#lfc <- res_sign[,2] # res_sign from DE_analysis.R 
-#names(lfc) <- row.names(res_sign)
 
 for (pathway in pathways_to_analyse$V1){
   pv.out <- pathview(gene.data = lfc_all, pathway.id = pathway, gene.idtype = 'kegg',
@@ -31,13 +30,12 @@ pv.out <- pathview(gene.data = lfc_all, pathway.id = "03008", gene.idtype = 'keg
                    species = "ko", out.suffix = "test6", limit = list(gene=2),
                    bins = list(gene=30))
 
-# species
-
+### Pathway analysis for all three species together
 lfc_all_gla <- lfc_all
 lfc_all_ecy <- lfc_all
 lfc_all_eve <- lfc_all
 
-save(lfc_all_gla, lfc_all_ecy, lfc_all_eve, file = 'lfc_all_species.RData')
+#save(lfc_all_gla, lfc_all_ecy, lfc_all_eve, file = 'lfc_all_species.RData')
 load('labeglo2/Transcriptomics/annotations/pathway_analisys/lfc_all_species.RData')
 
 lfc_all_species <- full_join(data.frame(x = lfc_all_gla, y = names(lfc_all_gla))[-1,], 
