44a0ffa87dfb4f78a7e34b24567301df9d26bc72
PolinaLip <polina.lipaeva@gmail.com>
2020-07-30 17:02:47 +0200

    Cleaning up

--- DE_analysis.R
+++ DE_analysis.R
@@ -17,8 +17,8 @@ path_to_files <- function(dir_to_samples, sample_dir_names){
 }
 
 dir <- 'labeglo2/Transcriptomics/quantification/AEA_metazoa_Gla' # specify path to your samples
-dir <- 'labeglo2/Transcriptomics/quantification/AEA_GlaReads_EcyAssembly/GlaReads_EcyAssembly'
-dir <- 'labeglo2/Transcriptomics/quantification/GlaReads_EcyAssembly_all_samples/'
+#dir <- 'labeglo2/Transcriptomics/quantification/AEA_GlaReads_EcyAssembly/GlaReads_EcyAssembly'
+#dir <- 'labeglo2/Transcriptomics/quantification/GlaReads_EcyAssembly_all_samples/'
 
 #files_names <- c('350', '351',  '352', '354', '632', '633',  '634',  '636',
 #                 'Eve_6C_rep1', 'Eve_6C_rep3', 'Eve_6C_rep4',
@@ -27,10 +27,10 @@ dir <- 'labeglo2/Transcriptomics/quantification/GlaReads_EcyAssembly_all_samples
 files_names <- c('311', '312', '313', '315', '594', '595', '596', '598') # specify the names of folders with quant.sf data, Gla
 files_names <- c('129', '130', '131', '133', '560', '561', '562', '563') # Ecy
 files_names <- c('350', '351',  '352', '354', '632', '633',  '634',  '636') # Eve
-files_names <- c('594_glaA', '595_glaA', '596_glaA', '598_glaA', 
-                 '560_glaA', '561_glaA', '562_glaA', '563_glaA') # Gla 1.5 and Ecy 1.5
-files_names <- c('594_ecyA', '595_ecyA', '596_ecyA', '598_ecyA', 
-                 '560_ecyA', '561_ecyA', '562_ecyA', '563_ecyA')
+#files_names <- c('594_glaA', '595_glaA', '596_glaA', '598_glaA', 
+#                 '560_glaA', '561_glaA', '562_glaA', '563_glaA') # Gla 1.5 and Ecy 1.5
+#files_names <- c('594_ecyA', '595_ecyA', '596_ecyA', '598_ecyA', 
+#                 '560_ecyA', '561_ecyA', '562_ecyA', '563_ecyA')
 files <- path_to_files(dir, files_names)
 all(file.exists(files))
 
@@ -50,14 +50,15 @@ txi_creation <- function(dir_to_samples,
 }
 
 txi <- txi_creation(dir, 'contigs_functional_annot.txt', files)
-txi <- txi_creation(dir, 'contigs_DAnnotation.txt', files)
-txi <- txi_creation(dir, 'Gla_kegg_pred_cds_annotation_wo_p_and_dupl.txt', files)
-txi <- txi_creation(dir, 'kegg_annot_Gla_assembly.txt', files)
-txi <- txi_creation(dir, 'pred_peptides_functional_annot_wo_p_and_Dupl.txt', files)
-txi$counts <- txi$counts[-1,]
+#txi <- txi_creation(dir, 'contigs_DAnnotation.txt', files)
+#txi <- txi_creation(dir, 'Gla_kegg_pred_cds_annotation_wo_p_and_dupl.txt', files)
+#txi <- txi_creation(dir, 'kegg_annot_Gla_assembly.txt', files)
+#txi <- txi_creation(dir, 'pred_peptides_functional_annot_wo_p_and_Dupl.txt', files)
+#txi$counts <- txi$counts[-1,]
 
 #write.table(txi$counts, 'labeglo2/Transcriptomics/quantification/AEA_metazoa_Ecy/txi_count_1.5_12.tsv', 
 #            sep='\t', quote = F)
+
 ### Prepare table with information about samples (it needs for DESeq running)
 
 table_with_sample_descr <- function(samples_number, conditions){ # conditions is a vector of conditions in the order: untreated, treated
@@ -70,19 +71,19 @@ table_with_sample_descr <- function(samples_number, conditions){ # conditions is
 }
 
 sample_description <- table_with_sample_descr(8, c('12C', '1.5C'))
-sample_description <- table_with_sample_descr(8, c('Gla', 'Ecy'))
-sample_description$condition <- factor(sample_description$condition, 
-                                       levels = c('Ecy', 'Gla'))
-sample_description$condition <- factor(sample_description$condition, 
-                                       levels = c('1.5C', '12C', '6C', '24C'))
-sample_description$condition <- ifelse(sample_description$condition == '1.5C', 
-                                       '1.5C', '6or12C')
-sample_description$replicate <- c(paste0('12C_', 1:4), paste0('1.5C_', 1:4), 
-                                  paste0('6C_', 1:3), paste0('24C_', 1:4))
-sample_description$condition <- factor(sample_description$condition, 
-                                       levels = c('6or12C','1.5C'))
-sample_description <- sample_description[!rownames(sample_description) == 'sample10',]
-row.names(sample_description) <- paste0('sample', 1:7)
+#sample_description <- table_with_sample_descr(8, c('Gla', 'Ecy'))
+#sample_description$condition <- factor(sample_description$condition, 
+#                                       levels = c('Ecy', 'Gla'))
+#sample_description$condition <- factor(sample_description$condition, 
+#                                       levels = c('1.5C', '12C', '6C', '24C'))
+#sample_description$condition <- ifelse(sample_description$condition == '1.5C', 
+#                                       '1.5C', '6or12C')
+#sample_description$replicate <- c(paste0('12C_', 1:4), paste0('1.5C_', 1:4), 
+#                                  paste0('6C_', 1:3), paste0('24C_', 1:4))
+#sample_description$condition <- factor(sample_description$condition, 
+#                                       levels = c('6or12C','1.5C'))
+#sample_description <- sample_description[!rownames(sample_description) == 'sample10',]
+#row.names(sample_description) <- paste0('sample', 1:7)
 
 ### Run differential expression analysis based on the negative binominal distribution
 
@@ -93,47 +94,15 @@ deseq$condition
 deseq <- DESeq(deseq)
 res <- results(deseq, name = resultsNames(deseq)[2]) # results() extracts a results table with log2 fold changes, p values and adjusted p values  
 #res[grepl('XP_015433778.1|RNA01364.1|RNA33404.1', row.names(res)),] # select set of genes
-res[grepl('OXCT1|OXCT|SCOT|CG1140|catI', row.names(res), ignore.case = T),]
+
 res[grepl('CYB5R3|DIA1|Cyb5r3|MCR1|YKL150W|YKL605|CBR1|CBR|CYB5R', row.names(res), ignore.case = T),] # NADH-cytochrome b5 reductase -> one of the component of aerobic desaturation (PUFA synthesis)
 res[grepl('CYB5A|CYB5B|CYB5m|Omb5|CB5-A|N1949|YNL111C|Cyt-b5|CG2140', row.names(res), ignore.case = T),] # cytochrome b5 -> one of the component of aerobic desaturation (PUFA synthesis)
 res[grepl('FADS|SCD|des6|Delta6FAD|FAD2|desat|OLE1|fat-6', row.names(res), ignore.case = T),] # acyl-coa desaturases -> one of the component of aerobic desaturation (PUFA synthesis)
-res[grepl('GPT2|AGXT2|GFPT|OAT', row.names(res), ignore.case = T),]
-res[grepl('Vg', row.names(res), ignore.case = F),]
-res[grepl('GLUD|Gdh', row.names(res), ignore.case = T),]
-
-### Pathways
-
-kegg_names <- read.csv('labeglo2/Transcriptomics/annotations/pathway_analisys/Gla/endocytosis_kegg_names.txt', header = F) # file with list of kegg names from the pathway of interest
-# Endocytosis
-pathway_res <- res[grepl('AP2A2|arf-1.2|ARF4|ARFGEF1|ARPC2|ARPC4|ARPC5|CAPZB|CCDC53|CHMP2A|CHMP2B|CHMP6|CHMP7|CLINT1|DAB2|DNM2|dyn-1|efa-6|EHD4|EPN2|FAM21A|fgr|GBF1|HGS|Hsp68|HSPA1A|IST1|KIAA0196|PARD3|PDCD6IP|PIP5K1B|RAB10|RAB11A|RAB11FIP2|Rab3|Rab7|rab8a|SH3KBP1|SMAD2|SNX4|Sop2|TSG101|WASHC1|ZFYVE20', row.names(res)),]
-pathway_res <- res[grepl('ACD88959.1|ACI90359.1|ACO11926.1|ELU01524.1|ELU18512.1|GBL84746.1|GBM03826.1|GBM49310.1|KAA0187646.1|KAE9436284.1|KAE9529754.1|KAF0313898.1|KOF99293.1|KPM10047.1|KPP61432.1|KRZ20197.1|NP_001079320.1|NP_001171767.1|ODN03957.1|OQR75673.1|P21613.1|PRD35706.1|PVD38106.1|QGC85414.1|RMZ98648.1|RNA06970.1|RNA10777.1|RNA12143.1|RNA23416.1|RNA30539.1|RNA36530.1|RNA40271.1|RNA40936.1|RNA42646.1|RNA43200.1|RNA45177.1|RUS82360.1|RWS17700.1|TDH14235.1|XP_001634219.1|XP_001947218.2|XP_002108937.1|XP_002608872.1|XP_002734355.1|XP_003387703.1|XP_005092492.1|XP_005092554.1|XP_007260027.2|XP_007552695.1|XP_008150111.2|XP_009016976.1|XP_009051123.1|XP_009060803.1|XP_009063300.1|XP_011423452.1|XP_011435429.1|XP_011436985.1|XP_011448123.1|XP_011448246.1|XP_011682840.2|XP_013089460.1|XP_013091222.1|XP_013145319.1|XP_013387153.1|XP_013403219.1|XP_013410030.1|XP_013413854.1|XP_013414079.1|XP_014788045.1|XP_015723034.1|XP_015753214.1|XP_015784105.1|XP_015833074.1|XP_015911282.1|XP_015913996.1|XP_015923451.1|XP_015929739.1|XP_015930111.1|XP_017280720.1|XP_018011276.1|XP_018012068.1|XP_018015153.1|XP_018015160.1|XP_018015331.1|XP_018020317.1|XP_018020389.1|XP_018022226.1|XP_018024609.1|XP_018025977.1|XP_018323433.1|XP_018595174.1|XP_018902622.1|XP_019622749.1|XP_019635230.1|XP_019926929.1|XP_020389187.1|XP_020470088.1|XP_020615495.1|XP_020775282.1|XP_020897630.1|XP_021350910.1|XP_021371058.1|XP_021376808.1|XP_021938486.1|XP_022109833.1|XP_022120296.1|XP_022293720.1|XP_022299881.1|XP_022314593.1|XP_022344077.1|XP_022666255.1|XP_022700037.1|XP_023165599.2|XP_023220284.1|XP_023242330.1|XP_023706140.1|XP_023933170.1|XP_024920344.1|XP_025076692.1|XP_025081538.1|XP_025089563.1|XP_026470738.1|XP_026544589.1|XP_026809367.1|XP_027002072.1|XP_027049103.1|XP_027217697.1|XP_027232977.1|XP_028916161.1|XP_029467529.1|XP_029646664.1|XP_029939337.1|XP_029981088.1|XP_030847587.1|XP_030848930.1', row.names(res)),] 
-#write.table(pathway_res, file = file.path(dir, 'endocytosis_DE_genes_1.5vs12.csv'), quote = F, sep = '\t')
-pathway_res <- pathway_res[row.names(pathway_res) != 'OQR75673.1',]
-colnames(pathway_res)
-pathway_res_with_kegg <- read.csv('labeglo2/Transcriptomics/annotations/pathway_analisys/Gla/endocytosis_DE_genes_1.5vs12_KEGG_NAMES.csv', 
-                                  sep = '\t')
-# mTOR
-pathway_res <- res[grepl('AAB51350.1|ACD37540.1|ACD37572.1|ARU12812.1|CRL08365.1|NP_001164247.1|NP_001191427.1|OPJ87379.1|OQV24379.1|PSN51071.1|QBB01716.1|RNA11603.1|RNA22546.1|RNA33212.1|RUS76505.1|RWS16001.1|RWS29667.1|RXG67127.1|VVC28224.1|XP_005996387.1|XP_006132953.1|XP_006788409.1|XP_009067006.1|XP_012522263.1|XP_013387556.1|XP_013396754.1|XP_013407885.1|XP_013416422.1|XP_013416707.1|XP_013774923.1|XP_014673959.1|XP_014779040.1|XP_014783093.1|XP_015919227.1|XP_018019914.1|XP_018022048.1|XP_018022624.1|XP_018027170.1|XP_018319431.1|XP_018565875.1|XP_019644561.1|XP_019875529.1|XP_020942410.1|XP_021185112.1|XP_021343299.1|XP_021378961.1|XP_022249574.1|XP_022338000.1|XP_022341054.1|XP_022912893.1|XP_023705414.1|XP_023933170.1|XP_024892256.1|XP_025076692.1|XP_025078135.1|XP_025081196.1|XP_025098693.1|XP_025114172.1|XP_025711122.1|XP_026293028.1|XP_027018255.1|XP_027030209.1|XP_028147582.1|XP_028319273.1|XP_028404561.1|XP_029022103.1|XP_029634367.1|XP_029635867.1|XP_029838482.1|XP_030068144.1|XP_031350878.1', row.names(res)),]
-write.table(pathway_res, file = file.path(dir, 'mTOR_DE_genes_1.5vs12.csv'), quote = F, sep = '\t')
-
-# Ras
-pathway_res <- res[grepl('ACI90359.1|ACT88125.1|AEN94436.1|EFX88843.1|ELT88415.1|ELT92419.1|GBL91632.1|KHJ40867.1|OQR78650.1|QBB01734.1|RNA01031.1|RNA11603.1|RWS16372.1|TMW48634.1|XP_002425376.1|XP_002600468.1|XP_003387703.1|XP_006499573.1|XP_013089460.1|XP_013404807.1|XP_013407885.1|XP_013409440.1|XP_013410607.1|XP_014662426.1|XP_015247035.1|XP_015929739.1|XP_017893513.1|XP_018011064.1|XP_018015054.1|XP_018015883.1|XP_019622515.1|XP_019875529.1|XP_021341911.1|XP_023320056.1|XP_023706140.1|XP_023933170.1|XP_025076692.1|XP_025078135.1|XP_025112658.1|XP_025114649.1|XP_026084923.1|XP_026101014.1|XP_027018255.1|XP_027048263.1|XP_027210598.1|XP_028147582.1|XP_029634367.1|XP_029791923.1|XP_031350878.1', row.names(res)),]
-write.table(pathway_res, file = file.path(dir, 'ras_DE_genes_1.5vs12.csv'), quote = F, sep = '\t')
-
-# mRNA surveillance pathway
-pathway_res <- res[grepl('AEC22818.1|AXF35703.1|AXF35717.1|CAX73342.1|ELU14652.1|ETN63045.1|GBM57768.1|KAB7506016.1|KAE9424857.1|KAF0287225.1|KFM57642.1|KFM79198.1|KFP38056.1|RMZ95943.1|RNA03132.1|RNA16196.1|RNA27667.1|RNA37021.1|RNA37638.1|RUS80738.1|SVE91448.1|VDO99168.1|XP_002414033.1|XP_003402681.1|XP_003995043.1|XP_004549214.1|XP_006824033.1|XP_008944423.1|XP_009062899.1|XP_011415665.1|XP_013380004.1|XP_013384817.1|XP_013389129.1|XP_013410745.1|XP_013413581.1|XP_015930587.1|XP_016394645.1|XP_018006606.1|XP_018006695.1|XP_018010441.1|XP_018011000.1|XP_018013254.1|XP_018015639.1|XP_018016380.1|XP_018019677.1|XP_018021902.1|XP_018023050.1|XP_018024083.1|XP_018027579.1|XP_019629205.1|XP_019713648.1|XP_020455096.1|XP_021197009.1|XP_022256823.1|XP_022785339.1|XP_022799141.1|XP_023227579.1|XP_023231017.1|XP_023931847.1|XP_026287481.1|XP_026495179.1|XP_027036616.1|XP_027217163.1|XP_028809182.1|XP_029201949.1|XP_029351959.1|XP_029658526.1|XP_029843320.1|XP_029948262.1|XP_030285256.1', row.names(res)),]
-write.table(pathway_res, file = file.path(dir, 'RNAsurv_DE_genes_1.5vs12.csv'), quote = F, sep = '\t')
-
-### Paint Pathway
 
 res_sign <- subset(res, padj < 0.05)
 res_ordered <- res_sign[order(res_sign$log2FoldChange),]
 res_sign_lfc <- subset(res_sign, log2FoldChange > 2)
 rownames(res_sign_lfc)
-#res_sign_lfc_up <- subset(res_sign_lfc, log2FoldChange > 0)
-#res_sign_lfc_down <- subset(res_sign_lfc, log2FoldChange < 0)
-#write.table(res_sign_lfc_up, file = file.path(dir, 'FA_UP_0.001_lfc2_DE_genes_1.5vs12.csv'), quote = F, sep = '\t')
-#write.table(res_sign_lfc_down, file = file.path(dir, 'FA_DOWN_0.001_lfc2_DE_genes_1.5vs12.csv'), quote = F, sep = '\t')
 
 ### Calculate sample-to-sample distances and draw a heatmap of these distances
 
