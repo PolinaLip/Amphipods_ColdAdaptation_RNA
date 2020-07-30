66579c24d9631c89943e249ac685cf4061f78dc1
PolinaLip <polina.lipaeva@gmail.com>
2020-07-30 15:34:30 +0200

    Add pathway analisys

---
+++ pathway_analysis.R
@@ -0,0 +1,60 @@
+#BiocManager::install("pathview")
+library(pathview)
+library(dplyr)
+data(gse16873.d)
+
+# example from pathview
+pv.out <- pathview(gene.data = gse16873.d[, 1], pathway.id = "04110",
+                   species = "hsa", out.suffix = "gse16873")
+
+# analysis
+pathway_dir <- 'labeglo2/Transcriptomics/annotations/pathway_analisys/Gla/Gla_KEGG_annotation/kegg_annot_assembly/'
+#pathways_to_analyse <- read.table(file.path(pathway_dir, 'pathway_to_analyse2.txt'), 
+#                                sep = '\t', header = F, colClasses = "character")
+#pathways_to_analyse <- subset(pathways_to_analyse, V1 != '04723') # for Gla
+de_results <- res # res from DE_analysis.R
+lfc_all <- ifelse(de_results$padj > 0.05 | is.na(de_results$padj), 
+                  0, de_results$log2FoldChange)
+names(lfc_all) <- row.names(res)
+#lfc <- res_sign[,2] # res_sign from DE_analysis.R 
+#names(lfc) <- row.names(res_sign)
+
+for (pathway in pathways_to_analyse$V1){
+  pv.out <- pathview(gene.data = lfc_all, pathway.id = pathway, gene.idtype = 'kegg',
+                     species = "ko", out.suffix = "gla_all_padj0.05")
+}
+
+pv.out <- pathview(gene.data = lfc_all, pathway.id = "04723", gene.idtype = 'kegg',
+                   species = "ko", out.suffix = "test")
+
+pv.out <- pathview(gene.data = lfc_all, pathway.id = "03008", gene.idtype = 'kegg',
+                   species = "ko", out.suffix = "test6", limit = list(gene=2),
+                   bins = list(gene=30))
+
+# species
+
+lfc_all_gla <- lfc_all
+lfc_all_ecy <- lfc_all
+lfc_all_eve <- lfc_all
+
+save(lfc_all_gla, lfc_all_ecy, lfc_all_eve, file = 'lfc_all_species.RData')
+load('labeglo2/Transcriptomics/annotations/pathway_analisys/lfc_all_species.RData')
+
+lfc_all_species <- full_join(data.frame(x = lfc_all_gla, y = names(lfc_all_gla))[-1,], 
+                             data.frame(x = lfc_all_ecy, y = names(lfc_all_ecy)), 
+                             by = 'y')
+lfc_all_species <- full_join(lfc_all_species, 
+                             data.frame(x = lfc_all_eve, y = names(lfc_all_eve)),
+                             by = 'y')
+
+colnames(lfc_all_species) <- c('Gla', 'Kegg', 'Ecy', 'Eve')
+row.names(lfc_all_species) <- lfc_all_species$Kegg
+lfc_all_species <- lfc_all_species[,-2]
+
+pv.out <- pathview(gene.data = lfc_all_species, 
+                   pathway.id = "00472", gene.idtype = 'kegg',
+                   species = "ko", out.suffix = "all_species", limit = list(gene=1),
+                   bins = list(gene=20), multi.state = T, same.layer = T,
+                   keys.align = "y")
+
+
